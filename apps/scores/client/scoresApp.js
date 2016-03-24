80a54365484f5f617fee5e91430b63a0b59b6e11
Mark Swedberg <swedy13@gmail.com>
2016-03-24 16:06:41 -0500

    Scores App

--- apps/scores/client/scoresApp.js
+++ apps/scores/client/scoresApp.js
@@ -1,16 +1,19 @@
 // ON CREATED
 Template.ScoresApp.onCreated(function() {
   var self = this;
+  var user = Meteor.userId();
   self.resources = new ReactiveVar(null);
 
-  Meteor.call('getGamesAPI', function(error, result) {
-    if (!error) {
-      self.resources.set(result.games);
-    }
-    else {
-      console.log(error);
-    }
-  });
+  if (user) {
+    Meteor.call('getGamesAPI', function(error, result) {
+      if (!error) {
+        self.resources.set(result.games);
+      }
+      else {
+        console.log(error);
+      }
+    });
+  }
 });
 
 
@@ -18,8 +21,12 @@ Template.ScoresApp.onCreated(function() {
 Template.ScoresApp.helpers({
   getStats: function() {
     var self = Template.instance();
-    console.log(self.resources.get());
-    return self.resources.get();
+    var user = Meteor.userId();
+
+    if (user) {
+      console.log(self.resources.get());
+      return self.resources.get();
+    }
   }
 });
 
@@ -28,5 +35,12 @@ Template.ScoresApp.helpers({
 Template.ScoresApp.events({
   'click button': function(event) {
     event.preventDefault();
+
+    var n = [];
+    _.each($('ul.kda >li'), function(item) {
+      n = 0;
+      n++;
+    });
+    console.log(n);
   }
 });
